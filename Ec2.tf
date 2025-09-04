28d4b2a996455a174aae0732f525287ef70ccdc0
edikan24 <122684729+edikan24@users.noreply.github.com>
2025-09-03 22:48:23 -0700

    Update Ec2.tf

--- Ec2.tf
+++ Ec2.tf
@@ -1,1 +1,37 @@
+# Security Group
+resource "aws_security_group" "ec2_sg" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main_vpc.id
 
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "EC2SecurityGroup"
+  }
+}
+
+# Create 2 EC2 instances
+resource "aws_instance" "ec2_instance" {
+  count         = 2
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.main_subnet.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  tags = {
+    Name = "TerraformInstance-${count.index + 1}"
+  }
+}
