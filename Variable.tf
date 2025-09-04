018c06519d8450ed7144d25e31fa627363b07036
edikan24 <122684729+edikan24@users.noreply.github.com>
2025-09-03 22:41:28 -0700

    Update Variable.tf

--- Variable.tf
+++ Variable.tf
@@ -1,1 +1,24 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
 
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  default = "us-east-1a"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "Amazon Linux 2 AMI ID"
+  default     = "ami-0c02fb55956c7d316"
+}
